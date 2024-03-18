93fc6d6290ba9ee645f8d3ae0a77eeccbedcd8bb
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 09:58:45 +0530

    Colab_consolelog.java

--- src/test/java/Error/Console_log/Colab_consolelog.java
+++ src/test/java/Error/Console_log/Colab_consolelog.java
@@ -31,7 +31,7 @@ public class Colab_consolelog {
 		{
 			
 			  DesiredCapabilities dc = DesiredCapabilities.chrome();
-		        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+		        URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		        driver = new RemoteWebDriver(url, dc);
 		
 		       
